f467a621ada9cbf73520aa21f82e83f6f75b07f7
lixiang <csucoderlee@163.com>
2016-08-22 20:06:42 +0800

    将spring依赖配置在root下

---
+++ spring-web/src/main/java/org/csu/coderlee/config/WebConfig.java
@@ -0,0 +1,9 @@
+package org.csu.coderlee.config;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * Created by lixiang on 16/8/22.
+ */
+public class WebConfig extends WebMvcConfigurationSupport{
+}
